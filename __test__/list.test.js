30b4acf74d69f8c89199b283618c2dc84b5e5601
James McDaniel <icathaid@gmail.com>
2018-05-22 03:08:58 -0700

    fixing travis issue

--- __test__/list.test.js
+++ __test__/list.test.js
@@ -2,8 +2,8 @@ const list = require('../lib/list.js');
 
 describe('something', () => {
   it('should return 0', () => {
-      const expected = 0;
-      const actual = list();
-      expect(actual).toBe(expected);
+    const expected = 0;
+    const actual = list();
+    expect(actual).toBe(expected);
   });
 });
